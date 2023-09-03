f36a031c95e4fb633530165cbbbc325642c041f7
Mauro Dreissig <mukadr@gmail.com>
2023-09-02 22:17:32 -0300

    Introduce command line compiler shc
    It transpiles and invokes GCC.

---
+++ shc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Usage: shc <filename.shl>");
+    return;
+}
+
+var cCodeFileName = Path.ChangeExtension(args[0], ".c");
+var exeFileName = Path.ChangeExtension(args[0], ".exe");
+
+var emitter = new SharpCompiler.CodeGen.CCodeEmitter(new StreamWriter(cCodeFileName));
+
+var program = SharpCompiler.Parser.Parse(File.ReadAllText(args[0]));
+
+emitter.Generate(program);
+
+var gcc = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "gcc.exe",
+        Arguments = $"{cCodeFileName} -o {exeFileName}",
+        CreateNoWindow = true
+    }
+};
+
+gcc.Start();
